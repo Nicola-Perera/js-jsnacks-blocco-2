3192aff9fb5f2196ef60adebaa6c3d91a18ed9e2
Nicola-Perera <nicola.perera96@mail.com>
2021-10-25 11:52:10 +0200

    add function to find the higgest weight value

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,3 +17,36 @@ const bici4 = {
 }
 
 const bicycles = [bici1, bici2, bici3, bici4];
+const weightsList = [];
+for (let i = 0; i < bicycles.length; i++) {
+    const biciWeight = bicycles[i].peso
+   weightsList.push(biciWeight);
+}
+console.log(weightsList);
+
+const biciPiuPesante = piuPesante(weightsList);
+console.log(biciPiuPesante);
+
+// let heavyBici;
+// for (let i = 0; i < weightsList; i++) {
+//     if (weightsList[i] > weightsList[i++]) {
+//         heavyBici
+//     }
+// }
+ 
+// if (weightsList[i] > weightsList[i++]) [
+
+// ]
+
+function piuPesante (weightsList) {
+    let heavyBici;
+    for (let i = 0; i < weightsList.leght -1; i++) {
+        if (weightsList[i] > weightsList[i+1]) {
+            heavyBici = 1;
+           }
+           else {
+               heavyBici = i+1;
+           }
+        }
+        return heavyBici;
+    }
