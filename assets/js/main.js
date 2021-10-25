49899025061305deefdffded6ab82882c5bf9c95
Nicola-Perera <nicola.perera96@mail.com>
2021-10-25 12:21:18 +0200

    esercizio squadre: creata array  con le squadre

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,66 @@
-const bici1 = {
-    nome: 'primo',
-    peso: 30
-}
-
-const bici2 = {
-    nome: 'secondo',
-    peso: 21
-}
-const bici3 = {
-    nome: 'terzo',
-    peso: 43
-}
-const bici4 = {
-    nome: 'quarto',
-    peso: 32
-}
-
-const bicycles = [bici1, bici2, bici3, bici4];
-const weightsList = [];
-for (let i = 0; i < bicycles.length; i++) {
-    const biciWeight = bicycles[i].peso
-   weightsList.push(biciWeight);
-}
-console.log(weightsList);
-
-const biciPiuPesante = piuPesante(weightsList);
-console.log(biciPiuPesante);
-
-// let heavyBici;
-// for (let i = 0; i < weightsList; i++) {
-//     if (weightsList[i] > weightsList[i++]) {
-//         heavyBici
-//     }
+// const bici1 = {
+//     nome: 'primo',
+//     peso: 30
 // }
- 
-// if (weightsList[i] > weightsList[i++]) [
 
-// ]
+// const bici2 = {
+//     nome: 'secondo',
+//     peso: 21
+// }
+// const bici3 = {
+//     nome: 'terzo',
+//     peso: 43
+// }
+// const bici4 = {
+//     nome: 'quarto',
+//     peso: 32
+// }
 
-function piuPesante (weightsList) {
-    let heavyBici;
-    for (let i = 0; i < weightsList.leght -1; i++) {
-        if (weightsList[i] > weightsList[i+1]) {
-            heavyBici = 1;
-           }
-           else {
-               heavyBici = i+1;
-           }
-        }
-        return heavyBici;
+
+// for (let i = 0; i < bicycles.length; i++) {
+//     const biciWeight = bicycles[i].peso
+//    weightsList.push(biciWeight);
+// }
+// console.log(weightsList);
+
+// let heavyBici = weightsList[0];
+//     for (let i = 1; i < weightsList.leght; i++) {
+        
+//         if (heavyBici < weightsList[i]) {
+//             heavyBici = weightsList[i];
+//            }
+           
+//         }
+//         console.log(heavyBici)
+
+
+const squadre = [
+    {
+        nome:'Inter',
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+    {
+        nome:'Milan',
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+    {
+        nome:'Juventus',
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+    {
+        nome:'Lazio',
+        punti_fatti: 0,
+        falli_subiti: 0
+    },
+    {
+        nome:'Roma',
+        punti_fatti: 0,
+        falli_subiti: 0
     }
+]
+
+console.log(squadre);
+
