6f2ed4d23ff6afef556dbefe4fcd0680493a686f
Nicola-Perera <nicola.perera96@mail.com>
2021-10-25 11:31:13 +0200

    add bicycles objects

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const bici1 = {
+    nome: 'primo',
+    peso: 30
+}
+
+const bici2 = {
+    nome: 'secondo',
+    peso: 21
+}
+const bici3 = {
+    nome: 'terzo',
+    peso: 43
+}
+const bici4 = {
+    nome: 'quarto',
+    peso: 32
+}
+
